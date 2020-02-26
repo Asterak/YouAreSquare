140598838ae5d7fa7eb8d0bf34d28661781bdde1
Сергей Гупалов <asterak@gmail.com>
2020-02-26 18:08:57 +0300

    Add project files.

---
+++ YouAreSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YouAreSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSquare(1));
+            Console.WriteLine(IsSquare(25));
+            Console.WriteLine(IsSquare(3));
+            Console.WriteLine(IsSquare(0));
+            Console.WriteLine(IsSquare(597524));
+        }
+
+        public static bool IsSquare(int n) => (Math.Floor(Math.Sqrt(n)) * Math.Floor(Math.Sqrt(n))) == n ? true : false;
+        
+    }
+}
